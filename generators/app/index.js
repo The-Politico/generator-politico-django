7d283ba281cdd38f42aaa68cc1b7f5bdc2f90f66
Jon McClure <jon.r.mcclure@gmail.com>
2019-01-02 20:27:37 -0500

    start

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,19 +7,11 @@ module.exports = class extends Generator {
       name: 'appName',
       message: 'What\'s your Django app\'s name?',
       default: path.basename(path.resolve(process.cwd(), '..')),
-    }, {
-      name: 'staticUrl',
-      message: 'What\'s your STATIC_URL (w/out slashes)?',
-      default: 'static',
     }];
 
     return this.prompt(questions).then((answers) => {
       this.appName = answers.appName;
-      this.staticUrl = answers.staticUrl;
     });
-  }
-  subgen() {
-    this.composeWith('politico-interactives:linters');
   }
   writing() {
     // Package meta
@@ -28,49 +20,27 @@ module.exports = class extends Generator {
       this.destinationPath('package.json'), {
         appName: this.appName,
       });
-    // Gulp
-    this.fs.copy(
-      this.templatePath('gulpfile.js'),
-      this.destinationPath('gulpfile.js'));
-    this.fs.copy(
-      this.templatePath('gulp/index.js'),
-      this.destinationPath('gulp/index.js'));
-    this.fs.copy(
-      this.templatePath('gulp/tasks/dev.js'),
-      this.destinationPath('gulp/tasks/dev.js'));
-    this.fs.copyTpl(
-      this.templatePath('gulp/tasks/build.js'),
-      this.destinationPath('gulp/tasks/build.js'), {
-        appName: this.appName,
-      });
-    this.fs.copyTpl(
-      this.templatePath('gulp/server/server.js'),
-      this.destinationPath('gulp/server/server.js'), {
-        staticUrl: this.staticUrl,
-        appName: this.appName,
-      });
     // Assets
     this.fs.copy(
-      this.templatePath('src/js/main-app.js'),
-      this.destinationPath('src/js/main-app.js'));
+      this.templatePath('src/main.app.js'),
+      this.destinationPath('src/main.app.js'));
     this.fs.copy(
-      this.templatePath('src/scss/main.scss'),
-      this.destinationPath('src/scss/main.scss'));
+      this.templatePath('src/theme/base.scss'),
+      this.destinationPath('src/theme/base.scss'));
     // Webpack config
-    this.fs.copy(
-      this.templatePath('webpack-dev.config.js'),
-      this.destinationPath('webpack-dev.config.js'));
     this.fs.copyTpl(
-      this.templatePath('webpack-prod.config.js'),
-      this.destinationPath('webpack-prod.config.js'), {
+      this.templatePath('webpack.config.dev.js'),
+      this.destinationPath('webpack.config.dev.js'), {
+        appName: this.appName,
+      });
+    this.fs.copyTpl(
+      this.templatePath('webpack.config.prod.js'),
+      this.destinationPath('webpack.config.prod.js'), {
         appName: this.appName,
       });
     this.fs.copy(
-      this.templatePath('postcss.config.js'),
-      this.destinationPath('postcss.config.js'));
-    this.fs.copy(
-      this.templatePath('babelrc'),
-      this.destinationPath('./.babelrc'));
+      this.templatePath('eslintrc.json'),
+      this.destinationPath('.eslintrc.json'));
     this.fs.copy(
       this.templatePath('gitignore'),
       this.destinationPath('./.gitignore'));
---
+++ generators/app/templates/webpack.config.dev.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const glob = require('glob');
+const _ = require('lodash');
+const portfinder = require('portfinder');
+
+portfinder.basePort = 3000;
+
+const config = (env, argv, port) => ({
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: _.zipObject(
+    glob.sync('./src/main.*.js*').map(f => path.basename(f, path.extname(f))),
+    glob.sync('./src/main.*.js*').map(f => [
+      '@babel/polyfill',
+      'whatwg-fetch',
+      f,
+    ]),
+  ),
+  output: {
+    path: path.resolve(__dirname, '../static/<%= appName %>'),
+    filename: 'js/[name].js',
+  },
+  devServer: {
+    compress: true,
+    port,
+    open: true,
+    contentBase: false,
+    proxy: {
+      '/': {
+        target: 'http://localhost:8000',
+      },
+      // Uncomment if proxying websockets...
+      // '/ws': {
+      //   target: 'ws://localhost:8000/',
+      //   ws: true,
+      // },
+    },
+    publicPath: '/static/<%= appName %>/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/env', {
+                targets: {
+                  browsers: 'last 2 versions',
+                },
+              }],
+              '@babel/react',
+            ],
+            plugins: [
+              '@babel/proposal-class-properties',
+            ],
+          },
+        },
+      },
+      {
+        test: /theme.*\.s?css$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+          },
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        }],
+      }, {
+        test: /\.s?css$/,
+        exclude: /theme.*\.s?css$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            sourceMap: true,
+          },
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        }],
+      },
+    ],
+  },
+});
+
+module.exports = (env, argv) =>
+  portfinder.getPortPromise()
+    .then(port => config(env, argv, port));
